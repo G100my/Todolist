8f75c8e651a44ce9ef24a157e80f050f9611f81e
G100my <G100my.lan229@gmail.com>
2020-12-01 16:52:47 +0800

    move editIcon to icon.js

--- components/titleBlock.js
+++ components/titleBlock.js
@@ -1,4 +1,4 @@
-import { starEmptyIcon, starFillIcon } from "../icon/icon.js";
+import { starEmptyIcon, starFillIcon, editIcon } from "../icon/icon.js";
 
 // ==== TitleBlock
 function createTitleBlock() {
@@ -11,10 +11,12 @@ function createTitleBlock() {
 	// create checkBox
 	checkBoxContainer = document.createElement("label");
 	checkBoxContainer.className = "checkbox-container";
+
 	checkbox = document.createElement("input");
 	checkbox.className = 'checkbox';
 	checkbox.type = 'checkbox';
 	checkbox.checked = false;
+	
 	checkMark = document.createElement('span');
 	checkMark.className = "check-mark";
 	checkMark.onclick = function () {
@@ -26,7 +28,7 @@ function createTitleBlock() {
 	// create edit
 	edit = document.createElement('a');
 	edit.className = 'edit';
-	edit.innerHTML = '<svg width="24px" height="24px" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/><path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/></svg>';
+	edit.innerHTML = editIcon;
 	edit.onclick = function () {
 		let newInput, targetSpan;
 		targetSpan = this.nextElementSibling.nextElementSibling;

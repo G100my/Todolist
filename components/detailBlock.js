96ce9b849cfd0621c8691cad956ff34a938d0e06
G100my <G100my.lan229@gmail.com>
2020-12-02 18:14:20 +0800

    style: remove useless function

--- components/detailBlock.js
+++ components/detailBlock.js
@@ -41,12 +41,12 @@ function creatDetailBlock() {
 	detailBlock.append(div, file, comment);
 
 	// use it when create a new task
-	detailBlock.setDetail = function (pushData) {
-		deadlineDate.value = pushData.date;
-		deadlineTime.value = pushData.time;
-		// file.value = pushData.file;
-		comment.lastElementChild.value = pushData.comment;
-	}
+	// detailBlock.setDetail = function (pushData) {
+	// 	deadlineDate.value = pushData.date;
+	// 	deadlineTime.value = pushData.time;
+	// 	// file.value = pushData.file;
+	// 	comment.lastElementChild.value = pushData.comment;
+	// }
 
 	detailBlock.deadlineDate = deadlineDate;
 	detailBlock.deadlineTime = deadlineTime;

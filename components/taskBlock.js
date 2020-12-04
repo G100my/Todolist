10ed32730dffeb0a45775a547dd836427962aaff
G100my <G100my.lan229@gmail.com>
2020-12-04 20:17:03 +0800

    style: format code

--- components/taskBlock.js
+++ components/taskBlock.js
@@ -2,12 +2,12 @@ import createButtonBlock from "./buttonBlock.js";
 import creatDetailBlock from "./detailBlock.js";
 import createStatusBlock from "./statusBlock.js";
 import createTitleBlock from "./titleBlock.js";
-import { starFillIcon, starEmptyIcon } from "../icon/icon.js";
+import {starFillIcon, starEmptyIcon} from "../icon/icon.js";
 
 function createTask(incomeData) {
 	let task, titleBlock, statusBlock, detailBlock, buttonGroup, accordion;
-	
-	task = Object.assign(document.createElement('div'), {
+
+	task = Object.assign(document.createElement("div"), {
 		id: "",
 		isComplete: false,
 		isStar: false,
@@ -31,8 +31,8 @@ function createTask(incomeData) {
 			deadlineTime: this.deadlineTime,
 			file: this.file,
 			comment: this.comment,
-		}
-	}
+		};
+	};
 
 	if (incomeData) {
 		task = Object.assign(task, incomeData);
@@ -42,37 +42,37 @@ function createTask(incomeData) {
 	titleBlock.checkbox.addEventListener("input", function () {
 		task.isComplete = this.checked;
 		emitUpdate();
-	})
+	});
 	titleBlock.starButton.addEventListener("click", function () {
 		task.isStar = !task.isStar;
 		reRender("star");
 		emitUpdate();
-	})
+	});
 	titleBlock.taskMassage.addEventListener("input", function () {
 		task.taskTitle = this.textContent;
 		reRender("taskMassage");
 		emitUpdate();
-	})
+	});
 	titleBlock.editButton.addEventListener("click", function () {
-		accordion.hidden = !accordion.hidden
-	})
-	
+		accordion.hidden = !accordion.hidden;
+	});
+
 	statusBlock = createStatusBlock();
 	titleBlock.append(statusBlock);
 
 	detailBlock = creatDetailBlock();
 
-	function updateData () {
+	function updateData() {
 		task.taskTitle = titleBlock.taskMassage.textContent;
 		task.deadlineDate = detailBlock.deadlineDate.value;
 		task.deadlineTime = detailBlock.deadlineTime.value;
 		task.comment = detailBlock.comment.value;
 		reRender("all");
 		emitUpdate();
-		accordion.hidden = true
+		accordion.hidden = true;
 	}
 
-	function cancelDetail () {
+	function cancelDetail() {
 		titleBlock.taskMassage.textContent = task.taskTitle;
 		detailBlock.deadlineDate.value = task.deadlineDate;
 		detailBlock.deadlineTime.value = task.deadlineTime;
@@ -84,7 +84,7 @@ function createTask(incomeData) {
 	buttonGroup.submitButton.addEventListener("click", updateData);
 	buttonGroup.cancelButton.addEventListener("click", cancelDetail);
 
-	accordion = document.createElement('div')
+	accordion = document.createElement("div");
 	accordion.className = "accordion";
 	accordion.append(detailBlock, buttonGroup);
 	accordion.hidden = true;
@@ -93,36 +93,27 @@ function createTask(incomeData) {
 
 	reRender("all");
 
-
 	function emitUpdate() {
-		console.log(task)
+		// console.log(task)
 		const Data = task.getData();
-		const taskUpdateEvent = new CustomEvent("taskUpdate", {detail:Data});
+		const taskUpdateEvent = new CustomEvent("taskUpdate", {detail: Data});
 		window.dispatchEvent(taskUpdateEvent);
 	}
 
 	function reRender(handler) {
-		const handlerNames = [
-			"checkbox",
-			"star",
-			"taskMassage",
-			"deadlineDate",
-			"deadlineTime",
-			"comment",
-			"status",
-		]
+		const handlerNames = ["checkbox", "star", "taskMassage", "deadlineDate", "deadlineTime", "comment", "status"];
 		switch (handler) {
 			case "all":
 				handlerNames.forEach((caseName) => {
 					reRender(caseName);
-				})
+				});
 				break;
-			
+
 			case "checkbox":
 				titleBlock.checkbox.checked = task.isComplete;
 				break;
 			case "star":
-				titleBlock.starButton.innerHTML = (task.isStar) ? starFillIcon : starEmptyIcon;
+				titleBlock.starButton.innerHTML = task.isStar ? starFillIcon : starEmptyIcon;
 				break;
 			case "taskMassage":
 				titleBlock.taskMassage.textContent = task.taskTitle;
@@ -139,9 +130,9 @@ function createTask(incomeData) {
 			case "status":
 				statusBlock.setStatus(task.deadlineDate, task.deadlineTime, task.file, task.comment);
 				break;
-			
+
 			default:
-				console.log("error: " + handler)
+				console.log("error: " + handler);
 				break;
 		}
 	}

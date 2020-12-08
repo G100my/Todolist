62473033128967941379fdabf245044692946a14
G100my <G100my.lan229@gmail.com>
2020-12-09 00:03:10 +0800

    feat: add statusBlock test, refactor statusBlock

--- test/baseBlock.test.js
+++ test/baseBlock.test.js
@@ -1,7 +1,57 @@
+import createStatusBlock from '../components/statusBlock.js';
 import createDetailBlock from '../components/detailBlock.js';
-import { datetimeIcon, fileIcon, commentIcon } from "../icon/icon.js";
+import { datetimeIcon, fileIcon, commentIcon } from '../icon/icon.js';
 import createButtonBlock from '../components/buttonBlock.js';
 
+describe('status block', () => {
+	const statusBlock = createStatusBlock();
+
+	const templateString = `
+	<div class="status-block"></div>
+	`.replace(new RegExp(/(\r\n|\t|\n)/, 'g'), '');
+
+	test('plain DOM', () => {
+		expect(statusBlock.outerHTML).toMatch(templateString);
+	});
+
+	test('inject date', () => {
+		const date = '2020-02-20';
+		statusBlock.setStatus({ date });
+		expect(statusBlock.innerHTML).toMatch(`<span>${datetimeIcon} ${date}</span>`);
+	});
+
+	test('inject time', () => {
+		const time = '10:10:10';
+		statusBlock.setStatus({ time });
+		expect(statusBlock.innerHTML).toMatch(`<span>${datetimeIcon} ${time}</span>`);
+	});
+
+	test('inject date & time', () => {
+		const date = '2020-02-20';
+		const time = '10:10:10';
+		statusBlock.setStatus({ date, time });
+		expect(statusBlock.innerHTML).toMatch(`<span>${datetimeIcon} ${date} ${time}</span>`);
+	});
+
+	test('inject file', () => {
+		const file = 'pretend something...';
+		statusBlock.setStatus({ file });
+		expect(statusBlock.innerHTML).toMatch(`<span>${fileIcon}</span>`);
+	});
+
+	test('inject comment', () => {
+		const comment = 'pretend something...';
+		statusBlock.setStatus({ comment });
+		expect(statusBlock.innerHTML).toMatch(`<span>${commentIcon}</span>`);
+	});
+
+	test('inject date & comment', () => {
+		const date = '2020-02-20';
+		const comment = 'pretend something...';
+		statusBlock.setStatus({ date, comment });
+		expect(statusBlock.innerHTML).toMatch(`<span>${datetimeIcon} ${date}</span><span>${commentIcon}</span>`);
+	});
+});
 
 describe('detail block', () => {
 	const detailBlock = createDetailBlock();
@@ -32,7 +82,6 @@ describe('detail block', () => {
 	});
 });
 
-
 describe('button block', () => {
 	const buttonBlock = createButtonBlock();
 	// replace + regExp, contructor 可以 new RegExp('	', 'g') 這樣使用 如果忘記 tab 是什麼的話...
@@ -46,4 +95,3 @@ describe('button block', () => {
 		expect(buttonBlock.outerHTML).toMatch(templateString);
 	});
 });
-

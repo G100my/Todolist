254072a103b17950486e2c1bc45b6364e634d8dc
G100my <G100my.lan229@gmail.com>
2020-12-09 16:23:35 +0800

    move const values out of test function for sharing

--- test/taskTemplate.test.js
+++ test/taskTemplate.test.js
@@ -14,65 +14,64 @@ function findDifferance(elemnetString, templateString) {
 }
 
 describe('task block', () => {
-	
-	test('with income data', () => {
-		const incomeData = {
-			id: 1111111,
-			isComplete: true,
-			isStar: false,
-			taskTitle: "安安",
-			deadlineDate: "2020-02-20",
-			deadlineTime: "00:00:00",
-			file: null,
-			comment: "Hello",
-		}
-		const taskBlock = createTaskBlock(incomeData)
-		const templateString = `
-			<div id="${incomeData.id}" class="task-container" draggable="true">
-				<div class="title-block">
-					<label class="checkbox-container">
-						<input class="checkbox" type="checkbox">
-						<span class="check-mark"></span>
-					</label>
-					<a class="edit">${editIcon}</a>
-					<a class="star">${starEmptyIcon}</a>
-					<span>${incomeData.taskTitle}</span>
-					<div class="status-block">
-						<span>${datetimeIcon} 2020-02-20 00:00:00</span>
-						<span>${commentIcon}</span>
-					</div>
-				</div>
-
-				<div class="accordion" hidden="">
-					<div class="setting-block">
-						<div>
-							${datetimeIcon}
-							<span>Deadline</span>
-							<input type="date">
-							<input type="time">
-						</div>
-						<div>
-							${fileIcon}
-							<span>File</span>
-							<label class="file-upload">+<input type="file"></label>
-						</div>
-						<div>
-							${commentIcon}
-							<span>Comment</span>
-							<textarea placeholder="Type your memo here..." cols="30" rows="10"></textarea>
-						</div>
-					</div>
-
-					<div class="button-block">
-						<button class="cancel-button">Cancel</button>
-						<button class="submit-button">+ Save</button>
-					</div>
+	const incomeData = {
+		id: 1111111,
+		isComplete: true,
+		isStar: false,
+		taskTitle: "安安",
+		deadlineDate: "2020-02-20",
+		deadlineTime: "00:00:00",
+		file: null,
+		comment: "Hello",
+	}
+	const taskBlock = createTaskBlock(incomeData)
+	const templateString = `
+		<div id="${incomeData.id}" class="task-container" draggable="true">
+			<div class="title-block">
+				<label class="checkbox-container">
+					<input class="checkbox" type="checkbox">
+					<span class="check-mark"></span>
+				</label>
+				<a class="edit">${editIcon}</a>
+				<a class="star">${starEmptyIcon}</a>
+				<span>${incomeData.taskTitle}</span>
+				<div class="status-block">
+					<span>${datetimeIcon} 2020-02-20 00:00:00</span>
+					<span>${commentIcon}</span>
 				</div>
 			</div>
-		`.replace(new RegExp(/(\r\n|\t|\n)/, 'g'), '');
 
-		findDifferance(taskBlock.outerHTML, templateString)
-		expect(taskBlock.outerHTML).toMatch(templateString)
+			<div class="accordion" hidden="">
+				<div class="setting-block">
+					<div>
+						${datetimeIcon}
+						<span>Deadline</span>
+						<input type="date">
+						<input type="time">
+					</div>
+					<div>
+						${fileIcon}
+						<span>File</span>
+						<label class="file-upload">+<input type="file"></label>
+					</div>
+					<div>
+						${commentIcon}
+						<span>Comment</span>
+						<textarea placeholder="Type your memo here..." cols="30" rows="10"></textarea>
+					</div>
+				</div>
+
+				<div class="button-block">
+					<button class="cancel-button">Cancel</button>
+					<button class="submit-button">+ Save</button>
+				</div>
+			</div>
+		</div>
+	`.replace(new RegExp(/(\r\n|\t|\n)/, 'g'), '');
+
+	test('create with income data', () => {
+		findDifferance(taskBlock.outerHTML, templateString);
+		expect(taskBlock.outerHTML).toMatch(templateString);
 	})
 })
 

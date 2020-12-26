f04c959eb07a0145c7eb8a6d0a4f5cd4fa35ce76
G100my <G100my.lan229@gmail.com>
2020-12-26 13:38:35 +0800

    test: add store test

---
+++ test/store.test.js
@@ -0,0 +1,87 @@
+import {updateTaskList} from "../src/store/store.js";
+
+describe("store", () => {
+
+	const income = {
+		id: "123",
+		num: 123,
+		isStar: false,
+	}
+
+	test("origin is empty", () => {
+		let origin = []
+
+		expect( updateTaskList(origin, income) ).toEqual([{
+			id: "123",
+			num: 123,
+			isStar: false,
+		}]);
+	})
+
+	test("origin has same data id", () => {
+		let origin = [{
+			id: "123",
+			num: 0,
+			isStar: true,
+		}]
+
+		expect( updateTaskList(origin, income) ).toEqual([{
+			id: "123",
+			num: 123,
+			isStar: false,
+		}]);
+	})
+
+
+	test("origin has same data, but differance", () => {
+		let origin = [{
+			id: "123",
+			num: 0,
+			isStar: true,
+		}]
+
+		expect(
+			updateTaskList(origin, income)	
+		).toEqual([{
+			id: "123",
+			num: 123,
+			isStar: false,
+		}]);
+	})
+
+	test("origin has many differance data", () => {
+		let origin = [
+			{
+				id: "000",
+				num: 0,
+				isStar: true,
+			},
+			{
+				id: "111",
+				num: 0,
+				isStar: true,
+			}
+		]
+
+		expect(
+			updateTaskList(origin, income)	
+		).toEqual(
+			[
+				{
+					id: "000",
+					num: 0,
+					isStar: true,
+				},
+				{
+					id: "111",
+					num: 0,
+					isStar: true,
+				}, {
+					id: "123",
+					num: 123,
+					isStar: false,
+				}
+			]);
+	})
+
+});

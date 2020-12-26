f206cbe7724eec457986bfbd051886c545aae62e
G100my <G100my.lan229@gmail.com>
2020-12-26 13:37:12 +0800

    refactor: utility - findDifferance

--- test/findDifferance.js
+++ test/findDifferance.js
@@ -2,9 +2,11 @@ module.exports = function findDifferance(elemnetString, templateString) {
 	const length = elemnetString.length > templateString.length ? elemnetString.length : templateString.length;
 	for (let i = 0; i < length; i++) {
 		if (elemnetString[i] != templateString[i]) {
-			elemnetString = elemnetString.slice(0, i) + "\n\n" + elemnetString.slice(i, elemnetString.length)
-			templateString = templateString.slice(0, i) + "\n\n" + templateString.slice(i, templateString.length)
-			console.warn(`position: ${i}`, elemnetString, templateString);
+			elemnetString = elemnetString.slice(i, elemnetString.length)
+			templateString = templateString.slice(i, templateString.length)
+            console.warn(`position: ${i}`);
+            console.log(elemnetString)
+            console.warn(templateString)
 			break;
 		}
 	}
